824fa1a40618932abbe548b26542eaa6a4e8ae0e
Chase Adams <gh@cadams.io>
2026-03-30 09:05:10 -0700

    chore: rework solid

--- ssr-themes/src/solid-context.ts
+++
@@ -1,6 +0,0 @@
-import {createContext} from 'solid-js';
-import type {ThemeContextValue} from './solid-types';
-
-export const ThemeContext = createContext<
-  ThemeContextValue | undefined
->(undefined);
--- ssr-themes/src/solid.ts
+++ ssr-themes/src/solid.ts
@@ -1,5 +1,7 @@
+import type {Accessor, JSX} from 'solid-js';
 import {
   createComponent,
+  createContext,
   createEffect,
   createMemo,
   createSignal,
@@ -9,19 +11,69 @@ import {
   useContext,
 } from 'solid-js';
 import {isServer} from 'solid-js/web';
-import {ThemeContext} from './solid-context';
-import type {
-  ThemeContextValue,
-  ThemeProviderProps,
-  ThemeResult,
-  ThemeSetter,
-} from './solid-types';
 import type {
   CookieOptions,
   LightOrDark,
   LightOrDarkTuple,
+  ThemeOptions,
   WithSystem,
 } from './types';
+
+export type ThemeSetter<
+  TTheme extends string = LightOrDark,
+  TEnableSystem extends boolean = true,
+> = (
+  value:
+    | WithSystem<TTheme, TEnableSystem>
+    | ((
+        prev:
+          | WithSystem<TTheme, TEnableSystem>
+          | undefined,
+      ) => WithSystem<TTheme, TEnableSystem>),
+) => WithSystem<TTheme, TEnableSystem>;
+
+export interface ThemeResult<
+  TTheme extends string = LightOrDark,
+  TEnableSystem extends boolean = true,
+> {
+  themes: Accessor<
+    ReadonlyArray<WithSystem<TTheme, TEnableSystem>>
+  >;
+  forcedTheme: Accessor<TTheme | undefined>;
+  setTheme: ThemeSetter<TTheme, TEnableSystem>;
+  theme: Accessor<
+    WithSystem<TTheme, TEnableSystem> | undefined
+  >;
+  resolvedTheme: Accessor<
+    Exclude<TTheme, 'system'> | undefined
+  >;
+  colorScheme: Accessor<
+    TEnableSystem extends true
+      ? LightOrDark
+      : undefined
+  >;
+}
+
+export interface ThemeProviderProps<
+  TTheme extends string = LightOrDark,
+  TEnableSystem extends boolean = true,
+> extends ThemeOptions<TTheme, TEnableSystem> {
+  children?: JSX.Element;
+  disableTransitionOnChange?: boolean | undefined;
+  initialTheme?:
+    | WithSystem<TTheme, TEnableSystem>
+    | undefined;
+  nonce?: string;
+}
+
+export type ThemeContextValue = ThemeResult<
+  string,
+  boolean
+>;
+
+export const ThemeContext = createContext<
+  ThemeContextValue | undefined
+>(undefined);
 
 const colorSchemes = ['light', 'dark'];
 const MEDIA = '(prefers-color-scheme: dark)';
@@ -468,11 +520,6 @@ export const ThemeProvider = <
   });
 };
 
-export type {
-  ThemeProviderProps,
-  ThemeResult,
-  ThemeSetter,
-} from './solid-types';
 export type {
   Attribute,
   CookieOptions,
